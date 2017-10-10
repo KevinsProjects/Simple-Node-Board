0f3ee96babf7a670381f755972a97d2978563073
KevinsProjects <kevinlk29@gmail.com>
2017-10-10 00:50:07 -0700

    Added mock data for testing

---
+++ backend/mockData/opinions.js
@@ -0,0 +1,28 @@
+const opinions = [
+  {
+    'discussion_id': '58c641904e457708a7147417',
+    'user_id': '58c242e2fb2e150d2570e02b',
+    'date': 1486450269704,
+    'content': 'Awesome stuff!',
+  },
+  {
+    'discussion_id': '58c641904e457708a7147417',
+    'user_id': '58c242e2fb2e150d2570e02b',
+    'date': 1486450269704,
+    'content': 'Awesome stuff really!',
+  },
+  {
+    'discussion_id': '58c641cf88336b08c76f3b50',
+    'user_id': '58c242a96ba2030d170f86f9',
+    'date': 1486450269704,
+    'content': 'Great job!',
+  },
+  {
+    'discussion_id': '58c641cf88336b08c76f3b50',
+    'user_id': '58c242a96ba2030d170f86f9',
+    'date': 1486450269704,
+    'content': 'This discussion is mind blowing!',
+  },
+];
+
+module.exports = opinions;
